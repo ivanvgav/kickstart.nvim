d01f1f8a4d6ef679a64e70375f18725d06390f0a
ivan <soyivan@gmail.com>
2023-11-24 19:39:37 -0300

    Angular treesitter

--- init.lua
+++ init.lua
@@ -217,7 +217,7 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-  -- { 'nvim-treesitter/nvim-treesitter-angular' },
+  { 'nvim-treesitter/nvim-treesitter-angular' },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
